6ad3d3d41add500dbc06baf40d030a64a0fa5d9e
Wendell Li <wendellli1999@gmail.com>
2022-04-12 21:28:29 -0700

    Fix UI bug

--- INFOCAPSTONE/src/pages/index.tsx
+++ INFOCAPSTONE/src/pages/index.tsx
@@ -1,24 +1,30 @@
-import React from 'react'
-import styles from './index.css'
-import SearchBar from '../components/searchBar' 
+import React from 'react';
+import styles from './index.css';
+import SearchBar from '../components/searchBar';
 
 /**
  * 主页面page
- * @param props 
+ * @param props
  */
-function homePage(props:any){
-    return (
-        <div className={styles.contentWrapper}>
-          <div className={styles.logo}>
-              <img src={require('../assets/logo.jpeg')} alt='logo icon' width='42px' height='42px'/>
-              <p style={{margin: 0, color: '#1890FF'}}>Course Expert</p>
-          </div>
-          <div className={styles.barWrapper}>
-            <SearchBar width='100%'/>
-          </div>
-        </div>
-      );
+function homePage(props: any) {
+  return (
+    <div className={styles.contentWrapper}>
+      <div className={styles.logo}>
+        <img
+          src={require('../assets/logo.jpeg')}
+          alt="logo icon"
+          width="42px"
+          height="42px"
+        />
+        <p style={{ margin: 0, color: '#1890FF', fontSize: '32pt' }}>
+          Course Expert
+        </p>
+      </div>
+      <div className={styles.barWrapper}>
+        <SearchBar width="100%" />
+      </div>
+    </div>
+  );
 }
 
-export default homePage
-
+export default homePage;
