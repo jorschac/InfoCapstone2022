dd30928050bc9a9c8b24e6dc6f5c814ba8a2e1e5
Wendell Li <wendellli1999@gmail.com>
2022-04-13 20:39:35 -0700

    Some changes

--- INFOCAPSTONE/src/components/recommendationView/recommendationView.tsx
+++ INFOCAPSTONE/src/components/recommendationView/recommendationView.tsx
@@ -1,46 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RGL, { WidthProvider } from 'react-grid-layout';
+import RGL, { Responsive, WidthProvider } from 'react-grid-layout';
 
-import './base.css';
+import './style.css';
 
-const ReactGridLayout = WidthProvider(RGL);
+const ReactGridLayout = WidthProvider(Responsive);
 
 class MinMaxLayout extends React.PureComponent {
-  static defaultProps = {
-    isDraggable: true,
-    isResizable: true,
-    items: 5,
-    rowHeight: 30,
-    preventCollision: false,
-    compactType: null,
-    cols: 12,
-  };
-
   render() {
     return (
       <ReactGridLayout {...this.props}>
-        <div key="1" data-grid={{ x: 0, y: 0, w: 3, h: 3, static: false }}>
-          <span>MOVE</span>
+        <div key="1" data-grid={{ x: 0, y: 0, w: 3, h: 3, static: true }}>
+          <span>GRID 1</span>
         </div>
-        <div key="2" data-grid={{ x: 3, y: 0, w: 3, h: 3, static: false }}>
-          <span>MOVE</span>
+        {/* <div key="2" data-grid={{ x: 3, y: 0, w: 3, h: 3, static: true }}>
+          <span>GRID 2</span>
         </div>
         <div key="3" data-grid={{ x: 6, y: 0, w: 3, h: 3, static: true }}>
-          <span>STATIC</span>
-        </div>
+          <span>GRID 3</span>
+        </div> */}
       </ReactGridLayout>
     );
   }
 }
 
-//module.exports = MinMaxLayout;
-
-// if (require.main === module) {
-//   require('../test-hook.jsx')(module.exports);
-// }
-
-// const rootElement = document.getElementById('root');
-// ReactDOM.render(<MinMaxLayout />, rootElement);
-
-export default module.exports = MinMaxLayout;
+export default MinMaxLayout;
