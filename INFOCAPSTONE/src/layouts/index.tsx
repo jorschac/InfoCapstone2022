383bf52d5ec005eea94735b84b1b224f735d4f75
牛客945484650号 <zhaojiuzhou2015@outlook.com>
2022-05-25 15:13:53 -0700

    loading page

--- INFOCAPSTONE/src/layouts/index.tsx
+++ INFOCAPSTONE/src/layouts/index.tsx
@@ -3,17 +3,18 @@ import { history } from 'umi';
 import { useEffect, useState } from 'react';
 import styles from './index.css';
 import { connect } from 'dva';
-import NavBar from '@/components/navBar'
+import NavBar from '@/components/navBar';
 import axios from 'axios';
 import { Tag, Divider, Menu } from 'antd';
-import {AiFillGithub} from 'react-icons/ai'
+import { AiFillGithub } from 'react-icons/ai';
+//import loading from '@/assets/loading.gif'
 
 /**
  * courseInfo页面最上边的那个显示课程信息的组件
- * @param props 
+ * @param props
  */
 function headArea(props: any) {
-  const { updateProfessor, updateCourseCode, courseCode} = props;
+  const { updateProfessor, updateCourseCode, courseCode } = props;
   const initialState = {
     tags: [],
     course_code: '',
@@ -23,15 +24,18 @@ function headArea(props: any) {
   };
   const [state, setState] = useState(initialState);
   const [currentKey, setCurrentKey] = useState('detail');
-  useEffect(()=>{
+  useEffect(() => {
     //判断当前的路径是否合法，若不合法，则跳回home界面
-    if (['/courseInfo/detail'].includes(history.location.pathname) && !history.location.query.code) {
+    if (
+      ['/courseInfo/detail'].includes(history.location.pathname) &&
+      !history.location.query.code
+    ) {
       history.push('/');
     }
-    if(['/courseInfo/QA'].includes(history.location.pathname)) {
-      setCurrentKey('QA')
+    if (['/courseInfo/QA'].includes(history.location.pathname)) {
+      setCurrentKey('QA');
     }
-  })
+  });
   useEffect(() => {
     let code: string | string[] | null = history.location.query
       ? history.location.query.code
@@ -66,41 +70,65 @@ function headArea(props: any) {
     }
   };
 
-  return (
-    <div className={styles.backGroundPage}>
-      <div className={styles.navWrapper}>
-        <NavBar setTab={setCurrentKey}/>
-      </div>
-      <div className={styles.headArea}>
-        {/* <Divider style={{ marginBottom: '-2px', marginTop: '12px'}} /> */}
-        <h2 style={{ display: 'inline-block', padding: '30px 30px 0px' }}>
-          {state.course_code} {state.course_title} (
-          {state.credit == '' ? 'Na' : state.credit})
-        </h2>
-        <div style={{padding: '0 30px 15px'}}>
-        {tags}
+  if (state.course_code.length) {
+    return (
+      <div className={styles.backGroundPage}>
+        <div className={styles.navWrapper}>
+          <NavBar setTab={setCurrentKey} />
         </div>
-        <div style={{ width: '70%', marginBottom: '3vh' }}>
-          <p className={styles.courseDescribtion}>{state.description}</p>
+        <div className={styles.headArea}>
+          {/* <Divider style={{ marginBottom: '-2px', marginTop: '12px'}} /> */}
+          <h2 style={{ display: 'inline-block', padding: '30px 30px 0px' }}>
+            {state.course_code} {state.course_title} (
+            {state.credit == '' ? 'Na' : state.credit})
+          </h2>
+          <div style={{ padding: '0 30px 15px' }}>{tags}</div>
+          <div style={{ width: '70%', marginBottom: '3vh' }}>
+            <p className={styles.courseDescribtion}>{state.description}</p>
+          </div>
+          <div style={{ borderRadius: '14px' }}>
+            <Menu
+              onClick={clickHandler}
+              mode="horizontal"
+              selectedKeys={[currentKey]}
+              style={{ paddingLeft: '10px', borderRadius: '14px' }}
+            >
+              <Menu.Item key="detail">Course Overview</Menu.Item>
+              <Menu.Item key="QA">Discussion</Menu.Item>
+            </Menu>
+          </div>
         </div>
-        <div style={{borderRadius: '14px'}}>
-        <Menu
-          onClick={clickHandler}
-          mode="horizontal"
-          selectedKeys={[currentKey]}
-          style={{ paddingLeft: '10px', borderRadius: '14px' }}
-        >
-          <Menu.Item key="detail">Course Overview</Menu.Item>
-          <Menu.Item key="QA">Discussion</Menu.Item>
-        </Menu>
+        <div className={styles.cardsContainer}>{props.children}</div>
+        <div style={{ height: '5vh', width: '10vw', margin: '0 auto' }}>
+          <AiFillGithub />
+          <a
+            style={{
+              color: '#060016',
+              paddingLeft: '5px',
+              position: 'relative',
+              top: '-2px',
+            }}
+            href="https://github.com/jorschac/InfoCapstone2022"
+          >
+            Visit our Github
+          </a>
         </div>
       </div>
-      <div className={styles.cardsContainer}>
-        {props.children}
+    );
+  } else {
+    return (
+      <div style={{width: '100%', height: '100%'}}>
+        <div className={styles.loader}>
+          <img
+            style={{ width: '20vw', height: '30vh' }}
+            src={require('@/assets/loading.gif')}
+            alt="loading"
+          />
+          <div style={{fontSize:'xx-large', color:'white', position:'relative', left:'9%'}}>Loading...</div>
+        </div>
       </div>
-      <div style={{height: '5vh', width: '10vw', margin:'0 auto'}}><AiFillGithub/><a style={{color: '#060016', paddingLeft: '5px', position:'relative', top:'-2px'}} href='https://github.com/jorschac/InfoCapstone2022'>Visit our Github</a></div>
-    </div>
-  );
+    );
+  }
 }
 
 const mapStateProps = (state: any) => {
@@ -119,10 +147,10 @@ const actionCreator = {
   },
   updateCourseCode: (payload: string) => {
     return {
-        type: 'courseInfo/updateCourseCode',
-        payload,
+      type: 'courseInfo/updateCourseCode',
+      payload,
     };
-  }
+  },
 };
 
 export default connect(mapStateProps, actionCreator)(headArea);
